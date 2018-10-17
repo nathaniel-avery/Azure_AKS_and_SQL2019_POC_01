ea7e3fbd75c0b4dfabfe8d87c6a386cc287e8dca
nathaniel-avery <navery@actionet.com>
2018-10-16 23:29:18 -0400

    Initial Version of file

---
+++ Enviornment_Config_01.ps1
@@ -0,0 +1,17 @@
+#Creates a three node Kubernetes cluster in AKS for hosting SQL Server 2019 CTP 2.0
+
+#Login to Azure
+az login
+
+#Create Resource Group
+az group create -n rg-azk8sdemo -l eastus --tags Project=AzureK8s_Demo
+
+#Create Azure K8s setup in the RG
+az aks create -n AzureK8s-Demo -g rg-azk8sdemo -c 1 --generate-ssh-keys
+
+#Get Credentials
+az aks get-credentials -n AzureK8s-Demo -g rg-azk8sdemo 
+
+#Add additional nodes count = 2 in this example
+az aks scale -n AzureK8s-Demo -g rg-azk8sdemo -c 2
+
